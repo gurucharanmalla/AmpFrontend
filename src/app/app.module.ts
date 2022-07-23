a59b50212321b5522deed890d30dff95a1445c8d
nagoorvalishaik <nagoorshaik817@gmail.com>
2022-07-23 17:36:14 +0530

    Created Ticket Folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import { CheckoutComponent } from './Ticket/checkout/checkout.component';
+import { CartComponent } from './Ticket/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckoutComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
